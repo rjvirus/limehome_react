8600db29bfb5dd25127b48bb71a227371667bcec
Issam Ghanmi <issam.ghanmi@limehome.de>
2021-05-27 18:10:38 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Show us what you are able to do! </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
