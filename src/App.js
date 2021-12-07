574896cc84ed7aa3c2f1926bcc05657b85512a7d
rjvirus <rahulvirus100@gmail.com>
2021-12-07 22:40:06 +0100

    add logo and fix background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [properties, setProperties] = useState(undefined);
+  const [wishlist, setWishlist] = useState(undefined);
+
+  useEffect(() => {
+    fetch("https://api.limehome.com/properties/v1/public/properties")
+    .then(res => res.json())
+    .then(data => {
+      if(data.success) {
+        setProperties(data.payload)
+      } else {
+        setProperties(null);
+      }
+    })
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Show us what you are able to do! </p>
+        <img id="app-logo" src="limehome_logo.png" />
       </header>
+      <div className="App-body">
+        {properties.map(data => {
+          return <p>data</p>
+        })}
+      </div>
     </div>
   );
 }
