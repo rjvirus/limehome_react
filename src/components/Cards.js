72a485e97e7162ee223f44c497f638a343cd6928
rjvirus <rahulvirus100@gmail.com>
2021-12-09 22:00:49 +0100

    cleanup and comment Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,22 +1,22 @@
-import Card from "./Card";
 import React from "react";
+import Card from "./Card";
 
-export default function Cards(props)  {
-    const { properties, favourites, setFavourites } = props;
-    return (
-      <div className="row">
-        {properties?.map((data) => {
-          const indexInDB = favourites.indexOf(data.id)
-          return (
-            <Card
-              key={data.id}
-              selected={indexInDB !== -1}
-              position={indexInDB}
-              updateFavourites={setFavourites}
-              {...data}
-            />
-          )
-        })}
-      </div>
-    )
-  }
+export default function Cards(props) {
+  const { properties, favourites, setFavourites } = props;
+  return (
+    <div className="row">
+      {properties?.map((data) => {
+        const savedIndex = favourites.indexOf(data.id); //checking if property is marked as favourite
+        return (
+          <Card
+            key={data.id}
+            selected={savedIndex !== -1}
+            position={savedIndex}
+            updateFavourites={setFavourites}
+            {...data}
+          />
+        )
+      })}
+    </div>
+  )
+}
