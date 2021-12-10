310d134ea516f52d10b38d93b30f223c7b3a2a85
rjvirus <rahulvirus100@gmail.com>
2021-12-11 00:23:36 +0100

    code cleanup scrolltotop

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { scrollToTop } from '../App';
 
 export default function Pagination(props) {
 	const { page, total, updatePage, show = true, disabled } = props;
 	const localPage = disabled ? 1 : page;
 	const localTotal = disabled ? 1 : total;
+
+	const onClickChange = (action) => {
+		scrollToTop();
+		if(action === 'prev') {
+			updatePage(localPage - 1)
+		} else {
+			updatePage(localPage + 1)
+		}
+	}
 
 	return show ? (
 		<div className='pagination items'>
@@ -11,13 +21,13 @@ export default function Pagination(props) {
 			<button
 				className='app-btn'
 				disabled={localPage === 1}
-				onClick={() => updatePage(localPage - 1)}
+				onClick={() => onClickChange('prev')}
 			>
 				{'< Prev'}
 			</button>
 			<button 
 				className='app-btn'
-				onClick={() => updatePage(localPage + 1)} 
+				onClick={() => onClickChange('next')} 
 				disabled={localPage === localTotal || localTotal === 0}
 			>
 				{'Next >'}
